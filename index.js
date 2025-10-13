54d62089cc4864f5e15588e724209636db2a497d
Karl James Molde <kjmolde@traxiontech.net>
2025-10-13 08:53:14 +0000

    index.js edited online with Bitbucket

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World running automatic deply!');
+  res.send('Aotumaction Deployment using Bitbucketv + Docker Hub in Ubuntu Server');
 });
 
 app.listen(PORT, () => {
